8f887d31b869c69fcf45f84b222bdd8182dd9a3f
Christoffer Winterkvist <chris@zenangst.com>
2015-06-16 09:59:56 +0200

    Make functions public

--- Source/Storage.swift
+++ Source/Storage.swift
@@ -31,14 +31,14 @@ public class Storage {
     return buildPath
   }
 
-  static func load(path: URLStringConvertible) -> AnyObject? {
+  public static func load(path: URLStringConvertible) -> AnyObject? {
     let loadPath = Storage.buildPath(path)
     return fileManager.fileExistsAtPath(loadPath)
       ? NSKeyedUnarchiver.unarchiveObjectWithFile(loadPath)
       : nil
   }
 
-  static func load(contentsAtPath path: URLStringConvertible, _ error: NSErrorPointer? = nil) -> String? {
+  public static func load(contentsAtPath path: URLStringConvertible, _ error: NSErrorPointer? = nil) -> String? {
     let loadPath = Storage.buildPath(path)
     let contents = NSString(contentsOfFile: loadPath,
       encoding: NSUTF8StringEncoding,
@@ -47,7 +47,7 @@ public class Storage {
     return contents as? String
   }
 
-  static func save(# object: AnyObject, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
+  public static func save(# object: AnyObject, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
     let savePath = Storage.buildPath(path, createPath: true)
     let data: NSData = NSKeyedArchiver.archivedDataWithRootObject(object)
     var error: NSError?
@@ -59,7 +59,7 @@ public class Storage {
     closure(error: error)
   }
 
-  static func save(# contents: String, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
+  public static func save(# contents: String, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
     let savePath = Storage.buildPath(path, createPath: true)
     var error: NSError?
 
