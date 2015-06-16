7c76405274053cbf2bfc795d6cafbe45a5969ab2
Christoffer Winterkvist <chris@zenangst.com>
2015-06-16 09:08:38 +0200

    Add testSaveAndLoadContentsToFile

--- Pod/Tests/Tests.swift
+++ Pod/Tests/Tests.swift
@@ -57,6 +57,17 @@ class Tests: XCTestCase {
     if loadedObject != nil {
       XCTAssertEqual(initialObject.property!, loadedObject!.property!)
     }
+  }
 
+  func testSaveAndLoadContentsToFile() {
+    let path = "Folder/test.txt"
+    let expectedString = "My string"
+
+    Storage.save(contents: expectedString, path) { error in
+      XCTAssertNil(error)
+    }
+
+    let loadedString = Storage.load(contentsAtPath: path)
+    XCTAssertEqual(expectedString, loadedString!)
   }
 }
