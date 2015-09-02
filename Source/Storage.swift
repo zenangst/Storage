2b5e08dc7a8a51065c9b1f458de0d74011e75f05
Christoffer Winterkvist <chris@zenangst.com>
2015-09-02 16:18:33 +0200

    Migrate to Swift 2

--- Source/Storage.swift
+++ Source/Storage.swift
@@ -20,14 +20,14 @@ public struct Storage {
     if path.string != Storage.applicationDirectory {
       buildPath = "\(Storage.applicationDirectory)/\(path.string)"
 
-      let folderPath = buildPath.stringByDeletingLastPathComponent
+      let folderPath = NSString(string: buildPath).stringByDeletingLastPathComponent
 
       if folderPath != Storage.applicationDirectory {
-        var error: NSError?
-        fileManager.createDirectoryAtPath(folderPath,
-          withIntermediateDirectories: true,
-          attributes: nil,
-          error: &error)
+        do {
+          try fileManager.createDirectoryAtPath(folderPath,
+            withIntermediateDirectories: true,
+            attributes: nil)
+        } catch { }
       }
     }
 
@@ -45,9 +45,11 @@ public struct Storage {
 
   public static func load(contentsAtPath path: URLStringConvertible, _ error: NSErrorPointer? = nil) -> String? {
     let loadPath = Storage.buildPath(path)
-    let contents = NSString(contentsOfFile: loadPath,
-      encoding: NSUTF8StringEncoding,
-      error: error != nil ? error! : nil)
+    let contents: NSString?
+    do {
+      contents = try NSString(contentsOfFile: loadPath,
+            encoding: NSUTF8StringEncoding)
+    } catch { contents = nil }
 
     return contents as? String
   }
@@ -63,8 +65,12 @@ public struct Storage {
     var object: AnyObject?
 
     if let data = load(dataAtPath: path) {
-      object = NSJSONSerialization.JSONObjectWithData(data,
-        options: .MutableContainers, error: nil)
+      do {
+        object = try NSJSONSerialization.JSONObjectWithData(data,
+          options: .MutableContainers)
+      } catch _ {
+        object = nil
+      }
     }
 
     return object
@@ -72,43 +78,56 @@ public struct Storage {
 
   // MARK: - Saving
 
-  public static func save(# object: AnyObject, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
+  public static func save(object  object: AnyObject, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
     let savePath = Storage.buildPath(path, createPath: true)
     let data: NSData = NSKeyedArchiver.archivedDataWithRootObject(object)
     var error: NSError?
 
-    data.writeToFile(savePath,
-      options: NSDataWritingOptions.DataWritingAtomic,
-      error: &error)
+    do {
+      try data.writeToFile(savePath,
+        options: NSDataWritingOptions.DataWritingAtomic)
+    } catch let error1 as NSError {
+      error = error1
+    }
 
     closure(error: error)
   }
 
-  public static func save(# contents: String, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
+  public static func save(contents  contents: String, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
     let savePath = Storage.buildPath(path, createPath: true)
     var error: NSError?
 
-    (contents as NSString).writeToFile(savePath, atomically: true, encoding: NSUTF8StringEncoding, error: &error)
+    do {
+      try (contents as NSString).writeToFile(savePath, atomically: true, encoding: NSUTF8StringEncoding)
+    } catch let error1 as NSError {
+      error = error1
+    }
 
     closure(error: error)
   }
 
-  public static func save(# data: NSData, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
+  public static func save(data  data: NSData, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
     let savePath = Storage.buildPath(path, createPath: true)
     var error: NSError?
 
-    data.writeToFile(savePath, options: .DataWritingAtomic, error: &error)
+    do {
+      try data.writeToFile(savePath, options: .DataWritingAtomic)
+    } catch let error1 as NSError {
+      error = error1
+    }
 
     closure(error: error)
   }
 
-  public static func save(# JSON: AnyObject, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
+  public static func save(JSON  JSON: AnyObject, _ path: URLStringConvertible = Storage.applicationDirectory, closure: (error: NSError?) -> Void) {
     var error: NSError?
 
-    if let data = NSJSONSerialization.dataWithJSONObject(JSON,
-      options: nil, error: &error) {
+    do {
+      let data = try NSJSONSerialization.dataWithJSONObject(JSON,
+        options: [])
         save(data: data, path, closure: closure)
-    } else {
+    } catch let error1 as NSError {
+      error = error1
       closure(error: error)
     }
   }
@@ -123,7 +142,9 @@ public struct Storage {
   public static func removeAtPath(path: URLStringConvertible, _ error: NSErrorPointer? = nil) {
     let loadPath = Storage.buildPath(path)
 
-    fileManager.removeItemAtPath(loadPath, error: error != nil ? error! : nil)
+    do {
+      try fileManager.removeItemAtPath(loadPath)
+    } catch { }
   }
 }
 
